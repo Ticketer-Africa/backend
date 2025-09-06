752099e4028629c4fc2586d33903c93816393945
Areola Kelvin Mayokun <kareola960@gmail.com>
2025-09-06 10:54:05 +0100

    Mail micro service

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [CloudinaryModule],
+  imports: [CloudinaryModule, HttpModule],
   providers: [MailService],
   exports: [MailService],
 })
