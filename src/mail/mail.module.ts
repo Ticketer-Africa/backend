c578f54b1c714561b632ba071ad73e7805aa9f1a
therealEmma <eozegbe68@gmail.com>
2025-09-06 20:42:38 +0100

    Still working on the admin dashboard

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios'; // ← import this
 import { MailService } from './mail.service';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [CloudinaryModule],
+  imports: [
+    CloudinaryModule,
+    HttpModule.register({  // ← register HttpService here
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   providers: [MailService],
   exports: [MailService],
 })
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable, Logger } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import {
